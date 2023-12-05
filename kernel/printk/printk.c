3bb347045780cd9ec37700880e5fed428ae7f2a1
John Ogness <john.ogness@linutronix.de>
2023-12-05 14:09:31 +0000

    printk: nbcon: Start printing threads
    If there are no boot consoles, the printing threads are started
    in early_initcall.
    
    If there are boot consoles, the printing threads are started
    after the last boot console has unregistered. The printing
    threads do not need to be concerned about boot consoles because
    boot consoles cannot register once a non-boot console has
    registered.
    
    Until a printing thread of a console has started, that console
    will print using atomic_write() in the printk() caller context.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2400,6 +2400,9 @@ asmlinkage int vprintk_emit(int facility, int level,
 		 *
 		 * - When this CPU is in panic.
 		 *
+		 * - When booting, before the printing threads have been
+		 *   started.
+		 *
 		 * - During shutdown, since the printing threads may not get
 		 *   a chance to print the final messages.
 		 *
@@ -2409,6 +2412,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 		 * with boot consoles.
 		 */
 		if (is_panic_context ||
+		    !printk_threads_enabled ||
 		    (system_state > SYSTEM_RUNNING)) {
 			nbcon_atomic_flush_pending();
 		}
@@ -3760,6 +3764,7 @@ EXPORT_SYMBOL(register_console);
 /* Must be called under console_list_lock(). */
 static int unregister_console_locked(struct console *console)
 {
+	bool is_boot_con = (console->flags & CON_BOOT);
 	bool found_legacy_con = false;
 	bool found_nbcon_con = false;
 	bool found_boot_con = false;
@@ -3830,6 +3835,15 @@ static int unregister_console_locked(struct console *console)
 		have_legacy_console = found_legacy_con;
 	if (!found_nbcon_con)
 		have_nbcon_console = found_nbcon_con;
+
+	/*
+	 * When the last boot console unregisters, start up the
+	 * printing threads.
+	 */
+	if (is_boot_con && !have_boot_console) {
+		for_each_console(c)
+			nbcon_kthread_create(c);
+	}
 
 	return res;
 }
